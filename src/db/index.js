33df6cadc9f05a45157cde1eab9ee0fd5900263c
utkarsh-choudhary <90688693+utkarsh-choudhary@users.noreply.github.com>
2024-09-11 14:13:17 +0530

    utils code discussion

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB= async()=>{
     try {
         const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(connectionInstance);
+        // console.log(connectionInstance);
         
         console.log(`\n mongodb connected !! DB host : ${connectionInstance.connection.host}`);
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler=()=>{
+
+}
+
+
+
+
+export {asyncHandler};
+
+// try catch method
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(func)=>{()=>{}}
+// const asyncHandler=(func)=>async()=>{}
+
+
+    // const asyncHandler=(fn)=> async(req,res,next)=>{
+    //     try {
+    //         await fn(req,res,next)
+    //     } catch (error) {
+    //         res.status(err.code || 500).json({
+    //             success:false,
+    //             message:error.message
+    //         })
+    //     }
+    // }
