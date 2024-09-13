a3873ab0f3fb799e4cfe69208468a837ddc3ef5f
utkarsh-choudhary <90688693+utkarsh-choudhary@users.noreply.github.com>
2024-09-13 12:24:14 +0530

    forgot to save the files

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,7 @@
-const asyncHandler=()=>{
-
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
 }
 
 
